3e99a039d7922331d1da7a2c1b5482a7e1b86f4a
jzpoteat <jzpoteat@gmail.com>
2024-11-25 13:20:20 -0500

    add base route

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import { VueRouterAutoImports } from 'unplugin-vue-router'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/kat-app/',
   plugins: [
     VueRouter(),
     vue(),
