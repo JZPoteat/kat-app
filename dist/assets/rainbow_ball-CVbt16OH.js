fbb64a84aa64fac989579be66d7a34d123e03196
Jake Poteat <jake.poteat@amwins.com>
2024-12-07 16:22:10 -0500

    removed dist

--- dist/assets/rainbow_ball-CVbt16OH.js
+++
@@ -1,1 +0,0 @@
-const a="/kat-app/assets/rainbow_ball-B3k2D-FX.jpg";export{a as default};
--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { VueRouterAutoImports } from 'unplugin-vue-router'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/kat-app',
   plugins: [
     VueRouter(),
     vue(),
