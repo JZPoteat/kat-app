611a56b4a178ffcad80b0080b2c6bddafea868a5
Jake Poteat <jake.poteat@amwins.com>
2024-12-07 21:34:35 -0500

    maybe worked???

---
+++ types/pages/Store.vue.d.ts
@@ -0,0 +1,19 @@
+declare const _default: import("vue").DefineComponent<{}, {
+    products: import("vue").Ref<{
+        id: number;
+        name: string;
+        description: string;
+        image: string;
+    }[], {
+        id: number;
+        name: string;
+        description: string;
+        image: string;
+    }[] | {
+        id: number;
+        name: string;
+        description: string;
+        image: string;
+    }[]>;
+}, {}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").PublicProps, Readonly<{}> & Readonly<{}>, {}, {}, {}, {}, string, import("vue").ComponentProvideOptions, true, {}>;
+export default _default;
