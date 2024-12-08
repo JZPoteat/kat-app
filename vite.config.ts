4e0259bad8e42d5a1ed7ec58ef435e62f5a05327
Jake Poteat <jake.poteat@amwins.com>
2024-12-07 20:57:50 -0500

    remove outdir

--- vite.config.ts
+++ vite.config.ts
@@ -9,9 +9,6 @@ import { VueRouterAutoImports } from 'unplugin-vue-router'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  build: {
-    outDir: 'dist'
-  },
   plugins: [
     VueRouter(),
     vue(),
