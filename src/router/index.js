d498720741410003489fd5a22c3bb4b47d4928ed
Jake Poteat <jake.poteat@amwins.com>
2024-12-24 19:50:17 -0500

    try to fix refresh issue

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { routes, handleHotUpdate } from 'vue-router/auto-routes'
+
 const router = createRouter({
-  history: createWebHistory(''),
+  history: createWebHashHistory(), // Change this to hash history
   routes
 })
+
 if (import.meta.hot) {
   handleHotUpdate(router)
 }
+
 export default router
